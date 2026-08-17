02a1ac1504b4c1b51ed6e21584d5559f6f052465
josiane-silwa <josiannesilwa@gmail.com>
2026-08-17 00:59:13 -0300

    Criação do arquivo que contem a criacao e descrição das ferramentas

---
+++ ferramentas.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor
+from ferramentas import criar_ferramentas
+import os
+
+# Inicia o app
+st.set_page_config(page_title="Agente Assistente de Dados com IA", layout="centered")
+st.title("🦜 Agente Assistente de análise de dados com IA")
+
+# Descrição da ferramenta
+st.info("""
+Este assistente utiliza um agente, criado com Langchain, para te ajudar a explorar, analisar e visualizar dados de forma interativa.
+Faça o upload de um arquivo CSV e você poderá:
+
+- 📄 **Gerar relatórios automáticos**:
+    - **Relatório de informações gerais**: apresenta a dimensão do DataFrame, nomes e tipos das colunas, contagem de dados nulos e duplicados, além de sugestões de tratamentos e análises adicionais.
+    - **Relatório de estatísticas descritivas**: exibe valores como média, mediana, desvio padrão, mínimo e máximo; identifica possíveis outliers e sugere próximos passos com base nos padrões detectados.
+
+- 🔎 **Fazer perguntas simples sobre os dados**: como "Qual é a média do tempo de entrega?", "Quantos registros existem para cada categoria?", ou "Qual é o valor máximo da coluna de vendas?".
+                
+- 📊 **Criar gráficos automaticamente** com base em perguntas em linguagem natural.
+
+Ideal para analistas, cientistas de dados e equipes que buscam agilidade e insights rápidos com apoio de IA.
+""")
