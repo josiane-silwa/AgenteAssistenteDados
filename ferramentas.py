61e76608b5b1c2c13a00642ffb9328e29c137a9d
josiane-silwa <josiannesilwa@gmail.com>
2026-08-17 01:46:06 -0300

    Criacao da ferramenta que gera um relatorio com resumo estatistico dos dados

--- ferramentas.py
+++ ferramentas.py
@@ -86,3 +86,46 @@ def informacoes_dataframe(pergunta: str, df: pd.DataFrame) -> str:
         })
 
     return resposta
+
+# Ferramenta para gerar um resumo estatístico completo e descritivo da base de dados
+@tool
+def resumo_estatistico(pergunta: str, df: pd.DataFrame) -> str:
+    """
+    Utilize esta ferramenta sempre que o usuário solicitar um resumo estatístico completo e descritivo da base de dados,
+    incluindo várias estatísticas (média, desvio padrão, mínimo, máximo etc.) e/ou múltiplas colunas numéricas.
+    Não utilize esta ferramenta para calcular uma única métrica como 'qual é a média de X' ou 'qual a correlação das variáveis'.
+    """
+    # Coleta de estatísticas descritivas
+    estatisticas = df.describe(include='number').transpose().to_string()
+    
+    # Prompt de resposta
+    template_resposta = PromptTemplate(
+        template="""
+        Você é um analista de dados encarregado de interpretar resultados estatísticos de uma base de dados
+        a partir de uma {pergunta} feita pelo usuário.
+
+        A seguir, você encontrará as estatísticas descritivas da base de dados:
+
+        ================= ESTATÍSTICAS DESCRITIVAS =================
+
+        {resumo}
+
+        ============================================================
+
+        Com base nesses dados, elabore um resumo explicativo com linguagem clara, acessível e fluida, destacando
+        os principais pontos dos resultados. Inclua:
+
+        1. Um título: ## Relatório de estatísticas descritivas
+        2. Uma visão geral das estatísticas das colunas numéricas
+        3. Um paráfrago sobre cada uma das colunas, comentando informações sobre seus valores.
+        4. Identificação de possíveis outliers com base nos valores mínimo e máximo
+        5. Recomendações de próximos passos na análise com base nos padrões identificados
+        """,
+        input_variables=["pergunta", "resumo"]
+    )
+
+    cadeia = template_resposta | llm | StrOutputParser()
+
+    resposta = cadeia.invoke({"pergunta": pergunta, "resumo": estatisticas})
+
+    return resposta
