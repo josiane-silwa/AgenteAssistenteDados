22ec5e0ac9833572abbace9f566fc7f5f7f87557
josiane-silwa <josiannesilwa@gmail.com>
2026-08-17 01:44:32 -0300

    Criacao da ferramenta que gera um relatorio com informacoes sobre o dataframe

--- ferramentas.py
+++ ferramentas.py
@@ -21,3 +21,68 @@ llm = ChatGroq(
     model_name="llama-3.3-70b-versatile",
     temperature=0
 )
+
+# Ferramenta para gerar um relatório de informações gerais sobre o DataFrame
+@tool
+def informacoes_dataframe(pergunta: str, df: pd.DataFrame) -> str:
+    """Utilize esta ferramenta sempre que o usuário solicitar informações gerais sobre o dataframe,
+        incluindo número de colunas e linhas, nomes das colunas e seus tipos de dados, contagem de dados
+        nulos e duplicados para dar um panomara geral sobre o arquivo."""
+
+    # Coleta de informações
+    shape = df.shape
+    columns = df.dtypes
+    nulos = df.isnull().sum()
+    nans_str = df.apply(lambda col: col[~col.isna()].astype(str).str.strip().str.lower().eq('nan').sum())
+    duplicados = df.duplicated().sum()
+
+   # Prompt de resposta 
+
+    template_resposta = PromptTemplate( 
+
+        template=""" 
+        Você é um analista de dados encarregado de apresentar um resumo informativo sobre um DataFrame 
+        a partir de uma {pergunta} feita pelo usuário. 
+
+        A seguir, você encontrará as informações gerais da base de dados: 
+
+        ================= INFORMAÇÕES DO DATAFRAME ================= 
+
+        Dimensões: {shape}
+ 
+        Colunas e tipos de dados: {columns} 
+
+        Valores nulos por coluna: {nulos} 
+
+        Strings 'nan' (qualquer capitalização) por coluna: {nans_str} 
+
+        Linhas duplicadas: {duplicados} 
+
+        ============================================================ 
+
+        Com base nessas informações, escreva um resumo claro e organizado contendo: 
+
+        1. Um título: ## Relatório de informações gerais sobre o dataset 
+        2. A dimensão total do DataFrame; 
+        3. A descrição de cada coluna (incluindo nome, tipo de dado e o que aquela coluna é) 
+        4. As colunas que contêm dados nulos, com a respectiva quantidade.  
+        5. As colunas que contêm strings 'nan', com a respectiva quantidade. 
+        6. E a existência (ou não) de dados duplicados. 
+        7. Escreva um parágrafo sobre análises que podem ser feitas com 
+        esses dados. 
+        8. Escreva um parágrafo sobre tratamentos que podem ser feitos nos dados. 
+        """, 
+        input_variables=["pergunta","shape", "columns", "nulos", "nans_str", "duplicados"] ) 
+
+    cadeia = template_resposta | llm | StrOutputParser()
+
+    resposta = cadeia.invoke({
+              "pergunta": pergunta,
+              "shape": shape,
+              "columns": columns,
+              "nulos": nulos,
+              "nans_str": nans_str,
+              "duplicados": duplicados
+        })
+
+    return resposta
